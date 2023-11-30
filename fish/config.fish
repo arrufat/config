d15f391cd498c7792a5c405e40ce9a9bbef7808a
Adrià Arrufat <adria.arrufat@outlook.com>
2023-11-30 15:13:24 +0900

    fish: update path and variables

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set -g -x NNN_PLUG ";:kak_open"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
