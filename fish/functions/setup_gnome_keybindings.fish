bfce4a0c89083ce4431f0b9cecc5aaee21c6c077
Adrià Arrufat <adria.arrufat@outlook.com>
2024-11-26 17:02:37 +0900

    fish: update gnome keybindings

--- fish/functions/setup_gnome_keybindings.fish
+++ fish/functions/setup_gnome_keybindings.fish
@@ -1,12 +1,15 @@
 function setup_gnome_keybindings
-    set keybindings org.gnome.desktop.wm.keybindings
-    gsettings set $keybindings close "['<Alt>F4', '<Super>c']"
-    gsettings set $keybindings begin-resize "['<Alt>F8', '<Super>r']"
-    gsettings set $keybindings toggle-fullscreen "['<Super>f']"
-    gsettings set $keybindings switch-to-workspace-1 "['<Super>Home', '<Super>1']"
-    gsettings set $keybindings move-to-workspace-1 "['<Super><Shift>Home', '<Shift><Super>1']"
+    set wm_keybindings org.gnome.desktop.wm.keybindings
+    set gs_keybindings org.gnome.shell.keybindings
+    gsettings set $wm_keybindings close "['<Alt>F4', '<Super>c']"
+    gsettings set $wm_keybindings begin-resize "['<Alt>F8', '<Super>r']"
+    gsettings set $wm_keybindings toggle-fullscreen "['<Super>f']"
+    gsettings set $wm_keybindings switch-to-workspace-1 "['<Super>Home', '<Super>1']"
+    gsettings set $gs_keybindings switch-to-application-1 "[]"
+    gsettings set $wm_keybindings move-to-workspace-1 "['<Super><Shift>Home', '<Shift><Super>1']"
     for i in (seq 2 9)
-        gsettings set $keybindings switch-to-workspace-$i "['<Super>$i']"
-        gsettings set $keybindings move-to-workspace-$i "['<Shift><Super>$i']"
+        gsettings set $wm_keybindings switch-to-workspace-$i "['<Super>$i']"
+        gsettings set $wm_keybindings move-to-workspace-$i "['<Shift><Super>$i']"
+        gsettings set $gs_keybindings switch-to-application-$i "[]"
     end
 end
